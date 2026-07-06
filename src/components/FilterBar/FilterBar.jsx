edc4dc5ebe2fc436152d629ae2531c3c4a4631f5
Nick Teo <nikostheodosis05@gmail.com>
2026-07-06 13:20:10 +0100

    feat: add FilterBar component

---
+++ src/components/FilterBar/FilterBar.jsx
@@ -0,0 +1,18 @@
+import "./FilterBar.css";
+
+export default function FilterBar({ categories, activeFilter, onFilterChange }) {
+  return (
+    <div className="filter-bar">
+      {categories.map((category) => (
+        <button
+          key={category.value}
+          type="button"
+          className={activeFilter === category.value ? "active" : ""}
+          onClick={() => onFilterChange(category.value)}
+        >
+          {category.label}
+        </button>
+      ))}
+    </div>
+  );
+}
